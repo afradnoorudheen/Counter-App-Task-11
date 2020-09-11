085ba37759e97d2b4abd927a96ec65279c074c9e
Afrad Noorudheen <58245117+afradnoorudheen@users.noreply.github.com>
2020-09-11 06:16:10 +0400

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+
 
 function App() {
+  const [count, setCount]=useState(0);
+  const clickPlus=(e)=>{
+       setCount(count+1);
+  };
+  const clickMinus=(e)=>{
+    setCount(count-1);
+};
+const clickReset=(e)=>{
+  setCount(0);
+};
+
+useEffect(() =>{
+  getLocalCount();
+},[]);
+
+
+useEffect(() =>{
+  
+  const saveLocalCount =() =>{
+    localStorage.setItem("count",JSON.stringify(count)); 
+  };
+  saveLocalCount();
+},[count]);
+
+const getLocalCount =() =>{
+  if(localStorage.getItem("count")==null){
+   localStorage.setItem("count",JSON.stringify([]));
+  }
+   else{
+    let countLocal= JSON.parse(localStorage.getItem('count'));
+    setCount(countLocal);
+   }
+  
+};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Counter App</h1>
+      
+    <div className="Counter">{count}</div>
+    <div className="ButtonContainer">
+    
+      <button className="Button Button1" onClick={clickMinus}>-
+      </button>
+      
+    
+      <button className="Button Button2" onClick={clickPlus}>+
+      </button>
+      
+    </div>
+    <button className="Button Reset" onClick={clickReset}>RESET
+      </button>
     </div>
   );
 }
